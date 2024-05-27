3c73b72b7c2179d30305f4bb0f26b41371c90b3d
hl3470 <hl3470@naver.com>
2024-05-27 15:25:46 +0900

    feat: Profit 테이블 구현

--- 4주차_프로젝트/fruit_calculator/fruit/models.py
+++ 4주차_프로젝트/fruit_calculator/fruit/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
+
 # Create your models here.
+class Profit(models.Model):
+    total_cost_price = models.IntegerField(null=False)
+    total_selling_price = models.IntegerField(null=False)
+    total_earning_price = models.IntegerField(null=False)
+    rate_of_return = models.DecimalField(null=False, max_digits=5, decimal_places=2)
+    create_date = models.DateTimeField(auto_now_add=True)
