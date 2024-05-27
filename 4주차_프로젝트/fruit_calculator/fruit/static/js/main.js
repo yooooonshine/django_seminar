ea00e239ee21f5b8f1804cb724d083546078475b
hl3470 <hl3470@naver.com>
2024-05-27 14:01:23 +0900

    feat: main.html, main.css, main.js 구현

---
+++ 4주차_프로젝트/fruit_calculator/fruit/static/js/main.js
@@ -0,0 +1,111 @@
+function getCookie(name) {
+    let cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+        const cookies = document.cookie.split(';');
+        for (let i = 0; i < cookies.length; i++) {
+            const cookie = cookies[i].trim();
+            // Does this cookie string begin with the name we want?
+            if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
+
+const csrftoken = getCookie('csrftoken');
+
+
+let fruitElement = "<li class=\"list-group-item\">\n" +
+    "            <select class=\"form-select w-25 d-inline\" aria-label=\"Default select example\">\n" +
+    "                <option value=\"1\">사과</option>\n" +
+    "                <option value=\"2\">귤</option>\n" +
+    "                <option value=\"3\">복숭아</option>\n" +
+    "                <option value=\"4\">메론</option>\n" +
+    "                <option value=\"5\">딸기</option>\n" +
+    "                <option value=\"6\">수박</option>\n" +
+    "            </select>\n" +
+    "\n" +
+    "            <input type=\"text\" placeholder=\"원가\">\n" +
+    "            <input type=\"text\" placeholder=\"판매가\">\n" +
+    "            <input type=\"text\" placeholder=\"수량\">\n" +
+    "        </li>";
+
+//과일 리스트 추가
+function addFruitList() {
+    $("#fruit-list").append(fruitElement);
+}
+
+//과일 계산하기
+function requestCalculate() {
+    let fruitsData = getFruitData();
+
+    let data = {
+        "fruits": fruitsData
+    }
+
+    $.ajax({
+        type: 'post',
+        headers: {'X-CSRFToken': csrftoken},
+        url: '/fruit/calculate',
+        body: JSON.stringify(data),
+        async: false,
+        dataType: 'JSON',
+        success: function (data) {
+            showResults(data.result)
+        },
+        error: function (request, status, error) {
+            if (request.status === 500) {
+                alert("서버가 작동하지 않습니다.");
+            } else {
+                alert(request.status + " 예외입니다.");
+            }
+        },
+    });
+}
+
+//입력된 과일 정보를 리스트로 가져오기
+function getFruitData() {
+    const fruitList = document.querySelectorAll('#fruit-list .list-group-item');
+    const fruitsData = [];
+
+    fruitList.forEach(item => {
+        const select = item.querySelector('select');
+        const inputs = item.querySelectorAll('input');
+
+        const fruit = {
+            "name": select.value,
+            "costPrice": inputs[0].value,
+            "sellingPrice": inputs[1].value,
+            "quantity": inputs[2].value
+        };
+
+        fruitsData.push(fruit);
+    });
+
+    return fruitsData;
+}
+
+// 응답된 계산 결과를 띄우기
+function showResults(result) {
+    $('#result-wrapper').css('display', 'block')
+
+    const totalCostPrice = result.totalCostPrice;
+    const totalSellingPrice = result.totalSellingPrice;
+    const totalEarningPrice = result.totalEarningPrice;
+    const netProfit = result.netProfit;
+    const rateOfReturn = result.rateOfReturn;
+
+    $('#total-cost-price').empty();
+    $('#total-selling-price').empty();
+    $('#total-earning-price').empty();
+    $('#net-profit').empty();
+    $('#rate-of-return').empty();
+
+    $('#total-cost-price').text(totalCostPrice);
+    $('#total-selling-price').text(totalSellingPrice);
+    $('#total-earning-price').text(totalEarningPrice);
+    $('#net-profit').text(netProfit);
+    $('#rate-of-return').text(rateOfReturn);
+}
